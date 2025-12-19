e48a8351479ffb728d908cde14733b676b7ab163
vaibhav <vaibhavnalla15@gmail.com>
2025-12-19 18:32:15 +0530

    Added etag files for s3 with terraform

---
+++ s3/etags/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "wick.txt"
+  source = "wick.txt"
+  etag = filemd5("wick.txt")
+}
