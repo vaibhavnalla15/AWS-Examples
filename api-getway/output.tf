80858d17298c2fdb3b215d156617dcd3b366eac4
vaibhav <vaibhavnalla15@gmail.com>
2026-01-28 23:02:28 +0530

    Added api-gateway + Lambda Examples

--- api-getway/output.tf
+++
@@ -1,5 +0,0 @@
-output "api_gateway_invoke_url" {
-  description = "The invoke URL of the API Gateway"
-  value       = "${aws_apigatewayv2_api.http_api.api_endpoint}/hello"
-}
-
--- api-getway/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_iam_role" {
-  description = "The ARN of the IAM role for the Lambda function"
-  type        = string
-}
